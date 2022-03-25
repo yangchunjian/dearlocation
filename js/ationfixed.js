09ee170ff0b6cc7b7b3c6898c59b88bb9f5ddb05
yangchunjian01 <yangchunjian01@58.com>
2022-03-25 23:23:18 +0800

    部署dearlocation.com

---
+++ js/ationfixed.js
@@ -0,0 +1,46 @@
+const rightBar = {
+  pcslides: [
+    {
+      title: '关注公众号',
+      desc: '一个走心,有温度的号,同千万同行一起交流学习',
+      imgUrl:
+        'https://www.yangchunjian.com/docbook/imgs/qrcode_for_gh_8756901e5b12_344.jpg',
+    },
+
+    {
+      title: '加作者微信',
+      desc: '扫二维码 备注 【加群】',
+      imgUrl:
+        'https://www.yangchunjian.com/docbook/imgs/dearlocation.jpeg',
+    },
+
+    {
+      title: '扫码易购',
+      desc: '福利推荐',
+      imgUrl:
+        'https://www.yangchunjian.com/docbook/imgs/qrcode.jpg',
+    },
+  ],
+
+  mobileslides: {
+    topImg:
+      'https://cdn.jsdelivr.net/gh/itclanCode/blogImgAssets/rightBarImgs/1606973305472-up-arrow.png',
+    newImg:
+      'https://cdn.jsdelivr.net/gh/itclanCode/blogImgAssets/rightBarImgs/1606971937519-new.png',
+    buyImg:
+      'https://www.yangchunjian.com/docbook/imgs/qrcode.jpg',
+    codeImg:
+      'https://www.yangchunjian.com/docbook/imgs/dearlocation.jpeg',
+    publicodeImg:
+      'https://www.yangchunjian.com/docbook/imgs/qrcode_for_gh_8756901e5b12_344.jpg',
+    adImg:
+      'https://cdn.jsdelivr.net/gh/itclanCode/blogImgAssets/rightBarImgs/1606973700328-ad.png',
+    shangImg:
+      'https://cdn.jsdelivr.net/gh/itclanCode/blogImgAssets/rightBarImgs/1606973752313-shang.png',
+    bottomImg:
+      'https://cdn.jsdelivr.net/gh/itclanCode/blogImgAssets/rightBarImgs/1606973784719-down-arrow.png',
+    keQunImg: 'https://static01.imgkr.com/temp/d9a2ba19f34a4468bac80bba9e437367.png'  
+  },
+};
+
+module.exports = rightBar;
