2cee5b185aee16799e521dbe6f5fcd49c3f05d14
yangchunjian <1091938307@qq.com>
2022-06-28 08:37:53 +0000

    Deploying to gh-pages from  @ d33c14be0aded874f9e09d74843f7c13d72ce9e1 🚀

---
+++ posts/article/java-project-1.html.vue
@@ -0,0 +1,23 @@
+<template><p>源码+文档+免费领取</p>
+<!-- more -->
+<p>Java练手项目 [第一弹]</p>
+<p><img src="https://tva2.sinaimg.cn/large/007F3CC8ly1h1l2mnl7y0j30oq09h76p.jpg" alt="image" loading="lazy"></p>
+<p>最近总有人私信我，问有没有Java练手的项目今天 yangchunjian 在这分享几个</p>
+<h2 id="_1-基于ssm图书馆管理系统" tabindex="-1"><a class="header-anchor" href="#_1-基于ssm图书馆管理系统" aria-hidden="true">#</a> 1. 基于SSM图书馆管理系统</h2>
+<p>项目地址：https://mp.weixin.qq.com/s/rydMsMCGZG5F-F-LpXS_0A</p>
+<h2 id="_2-​基于ssm-layui开发汽车租赁管理系统" tabindex="-1"><a class="header-anchor" href="#_2-​基于ssm-layui开发汽车租赁管理系统" aria-hidden="true">#</a> 2. ​基于ssm+layui开发汽车租赁管理系统</h2>
+<p>项目地址：https://mp.weixin.qq.com/s/zeKL8Ki9pvM3Yw9SwM7mug</p>
+<h2 id="_3-基于spring-boot2的个人博客系统" tabindex="-1"><a class="header-anchor" href="#_3-基于spring-boot2的个人博客系统" aria-hidden="true">#</a> 3. 基于spring boot2的个人博客系统</h2>
+<p>项目地址：https://mp.weixin.qq.com/s/WybspWYfy4SjhQrl71No5g</p>
+<h2 id="_4-16k点赞-基于spring-vue的前后端分离管理系统eladmin-真香" tabindex="-1"><a class="header-anchor" href="#_4-16k点赞-基于spring-vue的前后端分离管理系统eladmin-真香" aria-hidden="true">#</a> 4. 16K点赞 基于Spring + Vue的前后端分离管理系统ELAdmin，真香</h2>
+<p>项目地址：https://mp.weixin.qq.com/s/xW6H7Zv06jQYPbFPzHmcsw</p>
+<h2 id="_5-spring-boot-security-mybatis-thymeleaf-activiti-快速开发平台项目" tabindex="-1"><a class="header-anchor" href="#_5-spring-boot-security-mybatis-thymeleaf-activiti-快速开发平台项目" aria-hidden="true">#</a> 5. Spring Boot + Security + MyBatis + Thymeleaf + Activiti 快速开发平台项目</h2>
+<p>项目地址：https://mp.weixin.qq.com/s/2ER31ZqgakXM6HjHI7-ckg</p>
+<h2 id="_6-一款基于-spring-boot-的现代化社区-论坛-问答-社交网络-博客" tabindex="-1"><a class="header-anchor" href="#_6-一款基于-spring-boot-的现代化社区-论坛-问答-社交网络-博客" aria-hidden="true">#</a> 6. 一款基于 Spring Boot 的现代化社区（论坛/问答/社交网络/博客）</h2>
+<p>项目地址：https://mp.weixin.qq.com/s/iyaD_KL-QCmhkhdNgPcQlg</p>
+<p><strong>未完待续...</strong></p>
+<p>公众号二维码</p>
+<p><img src="/accounts/wechat.jpg" alt="" loading="lazy"></p>
+<p>个人微信</p>
+<p><img src="/accounts/QRcode.jpg" alt="" loading="lazy"></p>
+</template>
