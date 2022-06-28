8c31456b8c95cc67f9b07cc34eae89b022028ddb
yangchunjian <1091938307@qq.com>
2022-06-28 23:45:26 +0800

    添加搜索

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,10 +1,16 @@
+import { searchPlugin } from "@vuepress/plugin-search";
 import {defineUserConfig} from "vuepress";
 import {hopeTheme} from "vuepress-theme-hope";
 import * as navbar from "./navbar";
 import * as sidebar from "./sidebar";
 
 export default defineUserConfig({
+    plugins: [
+        searchPlugin({
+            // 你的选项
 
+        }),
+    ],
     theme: hopeTheme({
         title: "DearLocation",//默认信息
         description: "「Java面试+Java学习」",//默认信息
@@ -54,20 +60,6 @@ export default defineUserConfig({
                 `
             ],
         ],
-        // 此处放置主题配置
-        hostname: "https://dearlocation.com/",
-        author: {
-            name: "DearLocation.cn",
-            url: "https://dearlocation.com",
-        },
-        iconAssets: "iconfont",
-        iconPrefix: "iconfont icon-",
-        logo: "/dearlocation-backgroup.svg",
-        repo: "https://github.com/yangchunjian/dearlocation",
-        docsDir: "docs",
-        pageInfo: ["Author", "Original", "Date", "Category", "Tag", "ReadingTime"],
-        footer: "<a href='https://beian.miit.gov.cn/'>京ICP备xxx号</a>",
-        displayFooter: true,
         locales: {
             "/": {
                 // navbar
@@ -96,61 +88,76 @@ export default defineUserConfig({
                 },
             },
         },
-        encrypt: {
-            config: {
-                "/guide/encrypt.html": ["1234"],
-                "/zh/guide/encrypt.html": ["1234"],
-            },
+        // 此处放置主题配置
+        hostname: "https://dearlocation.com/",
+        author: {
+            name: "DearLocation.cn",
+            url: "https://dearlocation.com",
         },
-        plugins: {
-            //rss
-            feed: {
-                rss: true,
-            },
-            //搜索start
-            search: {
-                locales: {
-                    "/": {
-                        placeholder: "搜索",
-                    }
-                },
-            },
-            //搜索end
-            blog: {
-                autoExcerpt: true,
-            },
-            // If you don't need comment feature, you can remove following option
-            // The following config is for demo ONLY, if you need comment feature, please generate and use your own config, see comment plugin documentation for details.
-            // To avoid disturbing the theme developer and consuming his resources, please DO NOT use the following config directly in your production environment!!!!!
-            comment: {
-                /**
-                 * Using giscus
-                 */
-                // type: "giscus",
-                // repo: "yangchunjian/DearLocation-Blog",
-                // repoId: "R_kgDOHK-WNg",
-                // category: "Announcements",
-                // categoryId: "DIC_kwDOHK-WNs4COsXL",
-                /**
-                 * Using twikoo
-                 */
-                // type: "twikoo",
-                // envId: "https://twikoo.ccknbc.vercel.app",
-                /**
-                 * Using Waline
-                 */
-                type: "waline",
-                serverURL: "https://yangchunjian-java-pub-blog-cq4tgfqik-yangchunjian.vercel.app",
-                dark: "auto",//自适应深色模式
-                meta: ['nick', 'mail', 'link'],
-                login: "force",
-            },
-            mdEnhance: {
-                enableAll: true,
-                presentation: {
-                    plugins: ["highlight", "math", "search", "notes", "zoom"],
-                },
-            },
-        },
+        iconAssets: "iconfont",
+        iconPrefix: "iconfont icon-",
+        logo: "/dearlocation-backgroup.svg",
+        repo: "https://github.com/yangchunjian/dearlocation",
+        docsDir: "docs",
+        pageInfo: ["Author", "Original", "Date", "Category", "Tag", "ReadingTime"],
+        footer: "<a href='https://beian.miit.gov.cn/'>京ICP备xxx号</a>",
+        displayFooter: true,
+
+        // encrypt: {
+        //     config: {
+        //         "/guide/encrypt.html": ["1234"],
+        //         "/zh/guide/encrypt.html": ["1234"],
+        //     },
+        // },
+        // plugins: {
+        //     //rss
+        //     feed: {
+        //         rss: true,
+        //     },
+        //     //搜索start
+        //     search: {
+        //         locales: {
+        //             "/": {
+        //                 placeholder: "搜索",
+        //             }
+        //         },
+        //     },
+        //     //搜索end
+        //     blog: {
+        //         autoExcerpt: true,
+        //     },
+        //     // If you don't need comment feature, you can remove following option
+        //     // The following config is for demo ONLY, if you need comment feature, please generate and use your own config, see comment plugin documentation for details.
+        //     // To avoid disturbing the theme developer and consuming his resources, please DO NOT use the following config directly in your production environment!!!!!
+        //     comment: {
+        //         /**
+        //          * Using giscus
+        //          */
+        //         // type: "giscus",
+        //         // repo: "yangchunjian/DearLocation-Blog",
+        //         // repoId: "R_kgDOHK-WNg",
+        //         // category: "Announcements",
+        //         // categoryId: "DIC_kwDOHK-WNs4COsXL",
+        //         /**
+        //          * Using twikoo
+        //          */
+        //         // type: "twikoo",
+        //         // envId: "https://twikoo.ccknbc.vercel.app",
+        //         /**
+        //          * Using Waline
+        //          */
+        //         type: "waline",
+        //         serverURL: "https://yangchunjian-java-pub-blog-cq4tgfqik-yangchunjian.vercel.app",
+        //         dark: "auto",//自适应深色模式
+        //         meta: ['nick', 'mail', 'link'],
+        //         login: "force",
+        //     },
+        //     mdEnhance: {
+        //         enableAll: true,
+        //         presentation: {
+        //             plugins: ["highlight", "math", "search", "notes", "zoom"],
+        //         },
+        //     },
+        // },
     }),
 });
