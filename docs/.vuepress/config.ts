1fe63a1e07ac4547baa6bed1f1b066aee8300625
yangchunjian <1091938307@qq.com>
2022-06-29 20:11:26 +0800

    删除代码

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -8,52 +8,7 @@ import {commentPlugin} from "vuepress-plugin-comment2";
 
 export default defineUserConfig({
   // base,
-  head: [
-    ["link",
-      {
-        rel: "stylesheet",
-        href: "//at.alicdn.com/t/font_2410206_mfj6e1vbwo.css",
-      },
-    ],
-    //被搜索引擎搜索标签
-    ["meta",
-      {
-        name: 'keywords',
-        content: 'DearLocation,java,爬虫,毕业设计,python,计算机毕业设计,elasticsearch,springboot',
-      }
-    ],
-    //360站长的验证代码
-    ["meta",
-      {
-        name: "360-site-verification",
-        content: "63b223c45248d2ea87cb9b1042148dda",
-      },
-    ],
-    //百度站长的验证代码
-    ["meta",
-      {
-        name: "baidu-site-verification",
-        content: "code-jiOVohMv5f",
-      },
-    ],
-    // 添加360站长的自动推送代码
-    ["script",
-      {
-        src: "/assets/js/autopush-360.js",
-      },
-    ],
-    ['script', {},
-      `
-                  var _hmt = _hmt || [];
-                  (function() {
-                    var hm = document.createElement("script");
-                    hm.src = "https://hm.baidu.com/hm.js?469aa022c0181622bdf0357a8ca0fd9a";
-                    var s = document.getElementsByTagName("script")[0];
-                    s.parentNode.insertBefore(hm, s);
-                  })();
-                `
-    ],
-  ],
+
   // dest: "./dist",
 
   locales: {
