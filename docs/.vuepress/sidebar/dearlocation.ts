71bb43f0d4a61685f45194bd696aec5e81eb52bc
yangchunjian <1091938307@qq.com>
2022-06-30 14:01:43 +0800

    添加菜单

--- docs/.vuepress/sidebar/dearlocation.ts
+++ docs/.vuepress/sidebar/dearlocation.ts
@@ -2,7 +2,7 @@ import { sidebar } from "vuepress-theme-hope";
 
 export const dearlocation = sidebar({
   "/": [
-    "",
+    // "",
     // "home",
     // "slide",
     {
@@ -12,11 +12,47 @@ export const dearlocation = sidebar({
       // link: "exam/",
       collapsable: true,//可折叠
       children: "structure",
+    },{
+      text: "书籍",
+      icon: "creative",
+      prefix: "book/",
+      // link: "exa m/",
+      collapsable: true,//可折叠
+      children: "structure",
     },
     {
       text: "文章",
       icon: "creative",
       prefix: "posts/",
+      // link: "posts/",
+      collapsable: true,
+      children: "structure",
+
+    },
+    {
+      text: "财商",
+      icon: "creative",
+      prefix: "fq/",
+      // link: "posts/",
+      collapsable: true,
+      children: "structure",
+
+    },
+
+    {
+      text: "情商",
+      icon: "creative",
+      prefix: "eq/",
+      // link: "posts/",
+      collapsable: true,
+      children: "structure",
+
+    },
+
+    {
+      text: "智商",
+      icon: "creative",
+      prefix: "iq/",
       // link: "posts/",
       collapsable: true,
       children: "structure",
