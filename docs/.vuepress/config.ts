27c5e5f1b4856c1a8646f2921d735e871e95ff52
yangchunjian <1091938307@qq.com>
2022-06-30 15:09:50 +0800

    书籍

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,6 +2,7 @@ import { defineUserConfig } from "@vuepress/cli";
 import theme from "./theme";
 import { searchPlugin } from "@vuepress/plugin-search";
 import {commentPlugin} from "vuepress-plugin-comment2";
+import { componentsPlugin } from "vuepress-plugin-components";
 
 
 // const base = (process.env.BASE as "/" | `/${string}/`) || "/";
@@ -40,7 +41,19 @@ export default defineUserConfig({
       // 你的选项
 
     }),
-
+    componentsPlugin({
+      // addThis: "ra-5f829c59e6c6bc9a",
+      backToTop: true,
+      components: [
+        // "Badge",
+        // "CodePen",
+        // "FontIcon",
+        "PDF",
+        // "StackBlitz",
+        // "YouTube",
+      ],
+      // iconAssets: "iconfont",
+    }),
     // commentPlugin({
     //   // 插件选项
     //   provider: "Waline",
